60dee8edc1446fc29c0805008292400ecb9bfa08
hoang1998zl <hhoaang@gmail.com>
2023-07-15 15:56:50 +0700

    .

---
+++ src/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './assets/style.scss'
+import Seat from './components/Seat'
+
+const HomePage = () => {
+    return (
+        <div className='home'>
+            <Seat />
+        </div>
+    )
+}
+
+export default HomePage
